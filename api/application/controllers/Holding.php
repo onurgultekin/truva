219ab9a29802fe43a7141c32c6effb240ab43218
Onur Gültekin <coonur@gmail.com>
2017-08-11 15:12:26 +0300

    get holding(s) by id, country, city, county and area
    get holding(s) by id, country, city, county and area

---
+++ api/application/controllers/Holding.php
@@ -0,0 +1,52 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+class Holding extends REST_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->requestTime = date("Y-m-d H:i:s.u",round(microtime(true)));
+	}
+	function __destruct(){
+		parent::__destruct();
+		$url = $this->uri->uri_string();
+		$message = $this->output->final_output;
+		$userIp = $this->post("userIp");
+		$userId = $this->post("userId");
+		if(!$userId){
+			$userId = 0;
+		}
+		$responseTime = date("Y-m-d H:i:s.u",round(microtime(true)));
+		$this->insertLogs($message,$url,$userId,$this->requestTime,$responseTime);
+	}
+	function insertLogs($message,$method,$userId,$requestTime,$responseTime){
+		$this->db->close();
+		$message = json_decode($message);
+		$message = json_encode($message,JSON_UNESCAPED_UNICODE);
+		$query = $this->db->query("CALL INSERT_LOG('".md5($this->post("accessToken"))."','".$this->input->ip_address()."','".json_encode($this->post(),JSON_UNESCAPED_UNICODE)."','".$message."','".$method."',".$userId.",'".$requestTime."','".$responseTime."')");
+	}
+	public function getHoldingByCountry_post(){
+		$this->load->model("holding_model");
+		$message = $this->holding_model->getHoldingByCountry($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+	public function getHoldingByCity_post(){
+		$this->load->model("holding_model");
+		$message = $this->holding_model->getHoldingByCity($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+	public function getHoldingByCounties_post(){
+		$this->load->model("holding_model");
+		$message = $this->holding_model->getHoldingByCounties($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+	public function getHoldingByAreas_post(){
+		$this->load->model("holding_model");
+		$message = $this->holding_model->getHoldingByAreas($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+	public function getHoldinglist_post(){
+		$this->load->model("holding_model");
+		$message = $this->holding_model->getHoldinglist($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+}
---
+++ api/application/models/Holding_model.php
@@ -0,0 +1,232 @@
+<?php 
+class Holding_model extends CI_Model {
+
+        public function __construct()
+        {
+                parent::__construct();
+        }
+        public function getHoldingByCountry($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","countryId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","countryId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $countryId = $parameters["countryId"];
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if(!is_numeric($countryId)){
+                                        $response = $this->globalfunctions->returnMessage(1003,"Country Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL GET_HOLDING_BY_COUNTRY_ID('".$accessToken."',".$userId.",".$countryId.")");
+                                        $result = $query->row();
+                                        if(@$result->isError == 1){
+                                                $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                        }else{
+                                                $response["result"] = true;
+                                                $response["resultCode"] = 0;
+                                                $response["message"] = $query->result();
+                                        }
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function getHoldingByCity($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","cityId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","cityId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $cityId = $parameters["cityId"];
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if(!is_numeric($cityId)){
+                                        $response = $this->globalfunctions->returnMessage(1004,"City Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL GET_HOLDING_BY_CITY_ID('".$accessToken."',".$userId.",".$cityId.")");
+                                        $result = $query->row();
+                                        if(@$result->isError == 1){
+                                                $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                        }else{
+                                                $response["result"] = true;
+                                                $response["resultCode"] = 0;
+                                                $response["message"] = $query->result();
+                                        }
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function getHoldingByCounties($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","countyId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","countyId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $countyId = $parameters["countyId"];
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if(!is_numeric($countyId)){
+                                        $response = $this->globalfunctions->returnMessage(1005,"County Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL GET_HOLDING_BY_COUNTY_ID('".$accessToken."',".$userId.",".$countyId.")");
+                                        $result = $query->row();
+                                        if(@$result->isError == 1){
+                                                $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                        }else{
+                                                $response["result"] = true;
+                                                $response["resultCode"] = 0;
+                                                $response["message"] = $query->result();
+                                        }
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function getHoldingByAreas($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","areaId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","areaId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $areaId = $parameters["areaId"];
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if(!is_numeric($areaId)){
+                                        $response = $this->globalfunctions->returnMessage(1006,"Area Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL GET_HOLDING_BY_AREA_ID('".$accessToken."',".$userId.",".$areaId.")");
+                                        $result = $query->row();
+                                        if(@$result->isError == 1){
+                                                $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                        }else{
+                                                $response["result"] = true;
+                                                $response["resultCode"] = 0;
+                                                $response["message"] = $query->result();
+                                        }
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function getHoldinglist($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","holdingId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","holdingId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $holdingId = @$parameters["holdingId"];
+                                if(!$holdingId){
+                                        $holdingId = "NULL";
+                                }
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if($holdingId!="NULL" && !is_numeric($holdingId)){
+                                        $response = $this->globalfunctions->returnMessage(1006,"Holding Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL GET_HOLDINGS('".$accessToken."',".$userId.",".$holdingId.")");
+                                        $result = $query->row();
+                                        if(@$result->isError == 1){
+                                                $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                        }else{
+                                                $response["result"] = true;
+                                                $response["resultCode"] = 0;
+                                                $response["message"] = $query->result();
+                                        }
+                                }
+                        }
+                }
+                return $response;
+        }
+}
+?>
