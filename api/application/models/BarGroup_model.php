19aff0cda2e87e0786f0c8b5d2dcaffac7b18123
Onur Gültekin <coonur@gmail.com>
2017-08-12 15:17:49 +0300

    bar groups services implemented
    bar groups services implemented

---
+++ api/application/models/BarGroup_model.php
@@ -0,0 +1,171 @@
+<?php 
+class BarGroup_model extends CI_Model {
+
+        public function __construct()
+        {
+                parent::__construct();
+        }
+        
+        public function getBarGrouplist($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","BarGroupId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $BarGroupId = @$parameters["BarGroupId"];
+                                if(!$BarGroupId){
+                                        $BarGroupId = "NULL";
+                                }
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if($BarGroupId!="NULL" && !is_numeric($BarGroupId)){
+                                        $response = $this->globalfunctions->returnMessage(1006,"BarGroup Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL GET_BAR_GROUPS('".$accessToken."',".$userId.",".$BarGroupId.")");
+                                        $result = $query->row();
+                                        if(@$result->isError == 1){
+                                                $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                        }else{
+                                                $response["result"] = true;
+                                                $response["resultCode"] = 0;
+                                                $response["message"] = $query->result();
+                                        }
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function addBarGroup($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","Code","Name");
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","Code","Name");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $Code = $parameters["Code"];
+                                $Name = addslashes($parameters["Name"]);
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL ADD_OR_UPDATE_BAR_GROUP('".$accessToken."',".$userId.",NULL,'".$Code."','".$Name."')");
+                                        $result = $query->row();
+                                        $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function updateBarGroup($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","BarGroupID","Code","Name"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","BarGroupID","Code","Name");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $BarGroupID = $parameters["BarGroupID"];
+                                $Code = $parameters["Code"];
+                                $Name = addslashes($parameters["Name"]);
+                                if(!is_numeric($BarGroupID)){
+                                        $response = $this->globalfunctions->returnMessage(1009,"BarGroup Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL ADD_OR_UPDATE_BAR_GROUP('".$accessToken."',".$userId.",".$BarGroupID.",'".$Code."','".$Name."')");
+                                        $result = $query->row();
+                                        $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function deleteBarGroup($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","barGroupId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","barGroupId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $barGroupId = @$parameters["barGroupId"];
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if(!is_numeric($barGroupId)){
+                                        $response = $this->globalfunctions->returnMessage(1006,"BarGroup Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL DELETE_BAR_GROUP('".$accessToken."',".$userId.",".$barGroupId.")");
+                                        $result = $query->row();
+                                        $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                }
+                        }
+                }
+                return $response;
+        }
+}
+?>
