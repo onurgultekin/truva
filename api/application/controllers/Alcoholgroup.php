e13d16d2f06fa980a857910c1a6020c1dacde6f0
Onur Gültekin <coonur@gmail.com>
2017-08-12 12:42:04 +0300

    alcohol groups and types services implemented
    alcohol groups and types services implemented

---
+++ api/application/controllers/Alcoholgroup.php
@@ -0,0 +1,47 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+class Alcoholgroup extends REST_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->requestTime = date("Y-m-d H:i:s.u",round(microtime(true)));
+	}
+	function __destruct(){
+		parent::__destruct();
+		$url = $this->uri->uri_string();
+		$message = $this->output->final_output;
+		$userIp = $this->post("userIp");
+		$userId = $this->post("userId");
+		if(!$userId){
+			$userId = 0;
+		}
+		$responseTime = date("Y-m-d H:i:s.u",round(microtime(true)));
+		$this->insertLogs($message,$url,$userId,$this->requestTime,$responseTime);
+	}
+	function insertLogs($message,$method,$userId,$requestTime,$responseTime){
+		$this->db->close();
+		$message = json_decode($message);
+		$message = json_encode($message,JSON_UNESCAPED_UNICODE);
+		$query = $this->db->query("CALL INSERT_LOG('".md5($this->post("accessToken"))."','".$this->input->ip_address()."','".json_encode($this->post(),JSON_UNESCAPED_UNICODE)."','".addslashes($message)."','".$method."',".$userId.",'".$requestTime."','".$responseTime."')");
+	}
+	public function getAlcoholGrouplist_post(){
+		$this->load->model("AlcoholGroup_model");
+		$message = $this->AlcoholGroup_model->getAlcoholGrouplist($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+	public function addAlcoholGroup_post(){
+		$this->load->model("AlcoholGroup_model");
+		$message = $this->AlcoholGroup_model->addAlcoholGroup($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+	public function updateAlcoholGroup_post(){
+		$this->load->model("AlcoholGroup_model");
+		$message = $this->AlcoholGroup_model->updateAlcoholGroup($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+	public function deleteAlcoholGroup_post(){
+		$this->load->model("AlcoholGroup_model");
+		$message = $this->AlcoholGroup_model->deleteAlcoholGroup($this->post());
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+}
---
+++ api/application/models/AlcoholGroup_model.php
@@ -0,0 +1,171 @@
+<?php 
+class AlcoholGroup_model extends CI_Model {
+
+        public function __construct()
+        {
+                parent::__construct();
+        }
+        
+        public function getAlcoholGrouplist($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","alcoholGroupId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $alcoholGroupId = @$parameters["alcoholGroupId"];
+                                if(!$alcoholGroupId){
+                                        $alcoholGroupId = "NULL";
+                                }
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if($alcoholGroupId!="NULL" && !is_numeric($alcoholGroupId)){
+                                        $response = $this->globalfunctions->returnMessage(1006,"AlcoholGroup Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL GET_ALCOHOL_GROUPS('".$accessToken."',".$userId.",".$alcoholGroupId.")");
+                                        $result = $query->row();
+                                        if(@$result->isError == 1){
+                                                $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                        }else{
+                                                $response["result"] = true;
+                                                $response["resultCode"] = 0;
+                                                $response["message"] = $query->result();
+                                        }
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function addAlcoholGroup($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","Code","Name"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","Code","Name");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $Code = $parameters["Code"];
+                                $Name = addslashes($parameters["Name"]);
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL ADD_OR_UPDATE_ALCOHOL_GROUP('".$accessToken."',".$userId.",NULL,'".$Code."','".$Name."')");
+                                        $result = $query->row();
+                                        $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function updateAlcoholGroup($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","AlcoholGroupID","Code","Name"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","AlcoholGroupID","Code","Name");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $AlcoholGroupID = $parameters["AlcoholGroupID"];
+                                $Code = $parameters["Code"];
+                                $Name = addslashes($parameters["Name"]);
+                                if(!is_numeric($AlcoholGroupID)){
+                                        $response = $this->globalfunctions->returnMessage(1009,"AlcoholGroup Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL ADD_OR_UPDATE_ALCOHOL_GROUP('".$accessToken."',".$userId.",".$AlcoholGroupID.",'".$Code."','".$Name."')");
+                                        $result = $query->row();
+                                        $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                }
+                        }
+                }
+                return $response;
+        }
+        public function deleteAlcoholGroup($parameters){
+                $i = 0;
+                $k = 0;
+                $mandatoryParameters = array("accessToken","userId","alcoholGroupId"); 
+                foreach ($mandatoryParameters as $mandatoryParameter) {
+                        if(!array_key_exists($mandatoryParameter,$parameters)){
+                                $k++;
+                        }
+                } 
+                if($k>0){
+                        $response = $this->globalfunctions->returnMessage(1000,"Geçersiz istek. Zorunlu parametre eksik.",true);
+                }else{
+                        $availableParameters = array("accessToken","userId","alcoholGroupId");
+                        foreach ($parameters as $key => $parameter) {
+                                if(!in_array($key,$availableParameters)){
+                                        $i++;
+                                }
+                        }
+                        if($i>0){
+                                $response = $this->globalfunctions->returnMessage(1001,"Geçersiz istek. Bilinmeyen parametre girdiniz.",true);
+                        }else{
+                                $accessToken = $parameters["accessToken"];
+                                $userId = $parameters["userId"];
+                                $alcoholGroupId = @$parameters["alcoholGroupId"];
+                                if(!is_numeric($userId)){
+                                        $response = $this->globalfunctions->returnMessage(1002,"User Id parametresi numeric olmalıdır.",true);
+                                }else
+                                if(!is_numeric($alcoholGroupId)){
+                                        $response = $this->globalfunctions->returnMessage(1006,"AlcoholGroup Id parametresi numeric olmalıdır.",true);
+                                }else{
+                                        $query = $this->db->query("CALL DELETE_ALCOHOL_GROUP('".$accessToken."',".$userId.",".$alcoholGroupId.")");
+                                        $result = $query->row();
+                                        $response = $this->globalfunctions->returnMessage($result->responseCode,$result->responseMessage,@$result->isError);
+                                }
+                        }
+                }
+                return $response;
+        }
+}
+?>
